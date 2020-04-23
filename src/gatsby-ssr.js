f157f3b0373725f51d097f99e9af802a049fab0d
Maël Nison <nison.mael@gmail.com>
2020-04-23 16:00:15 +0200

    Adds support for multiple configurations

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -3,9 +3,21 @@ import { stripIndent } from "common-tags";
 
 exports.onRenderBody = (
   { setHeadComponents, setPostBodyComponents },
-  { apiKey, indexName, inputSelector, debug = false }
+  configuration,
 ) => {
-  if (!apiKey || !indexName || !inputSelector) {
+  if (!configuration) {
+    return;
+  }
+
+  if (!Array.isArray(configuration)) {
+    configuration = [configuration];
+  }
+
+  configuration = configuration.filter(spec => {
+    return spec.apiKey && spec.indexName && spec.inputSelector;
+  });
+
+  if (configuration.length === 0) {
     return;
   }
 
@@ -23,34 +35,36 @@ exports.onRenderBody = (
       type="text/javascript"
       src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"
     />,
-    <script
-      key="plugin-docsearch-initiate"
-      type="text/javascript"
-      dangerouslySetInnerHTML={{
-        __html: stripIndent`
-        var observer = new MutationObserver(function (mutations, instance) {
-          var docuSearchElem = document.querySelector('${inputSelector}');
-          if (docuSearchElem) {
-            docsearch({
-              apiKey: "${apiKey}",
-              indexName: "${indexName}",
-              inputSelector: "${inputSelector}",
-              debug: ${debug === true ? "true" : "false"}
-            });
-            instance.disconnect(); // stop observing
-            return;
-          }
-        });
-
-        // start observing
-        document.addEventListener("DOMContentLoaded", function() {
-          observer.observe(document, {
-            childList: true,
-            subtree: true
+    ...configuration.map(({ apiKey, indexName, inputSelector, debug = false }) => (
+      <script
+        key="plugin-docsearch-initiate"
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: stripIndent`
+          var observer = new MutationObserver(function (mutations, instance) {
+            var docuSearchElem = document.querySelector('${inputSelector}');
+            if (docuSearchElem) {
+              docsearch({
+                apiKey: "${apiKey}",
+                indexName: "${indexName}",
+                inputSelector: "${inputSelector}",
+                debug: ${debug === true ? "true" : "false"}
+              });
+              instance.disconnect(); // stop observing
+              return;
+            }
           });
-        });
-        `
-      }}
-    />
+
+          // start observing
+          document.addEventListener("DOMContentLoaded", function() {
+            observer.observe(document, {
+              childList: true,
+              subtree: true
+            });
+          });
+          `
+        }}
+      />
+    ),
   ]);
 };
